dcce5bb373bdc0d71158caac8ad5658620a74b48
David Loesch <davidloesch1@gmail.com>
2020-01-30 15:26:29 -0800

    got the get by id to work on comments

--- Controller/comments.js
+++ Controller/comments.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const router = express.Router();
-const mongoose = require("mongoose");
+
 
 const Comments = require("../Model/CommentsSchema");
 const Image = require("../Model/ImageSchema");
@@ -11,19 +11,24 @@ router.get("/", (req, res) => {
   Comments.find({}).then(comments => res.json(comments));
 });
 
+router.get("/:id", (req, res) => {
+  Comments.findById(req.params.id).then(comment => res.json(comment))
+});
+
+
 //this works
 router.post("/", (req, res) => {
+  console.log(req.body)
+  console.log(req.body.photo)
   Image.findById(req.body.photo).then(image => {
-      Comments.create(req.body).then(comment => {
-        image.comments.push(comment._id)
-        comment.photo = image._id
-        image.save()
-        comment.save()
-        res.json(image)
-      }
-  );
-  })
-
+    Comments.create(req.body).then(comment => {
+      image.comments.push(comment._id);
+      comment.photo = image._id;
+      image.save();
+      comment.save();
+      res.json(image);
+    });
+  });
 });
 
 //not working
