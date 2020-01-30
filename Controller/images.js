8f6a53ff3fb5529c1c6231b9ba2eb11ea9c9ea03
Phil Morales <pirs.hlpoae.iml@gmail.com>
2020-01-30 15:31:01 -0500

    final images coding for routes

--- Controller/images.js
+++ Controller/images.js
@@ -35,21 +35,7 @@ const Image = require("../Model/ImageSchema");
 
 router.get("/", (req, res, next) => {
   Image.find()
-    .select("title genre path _id")
-    .exec()
     .then(images => {
-    //  const res = {
-        // count: images.length,
-        // images: images.map(img => {
-          // return {
-          //   title: img.title,
-          //   genre: img.genre,
-          //   votes: img.votes,
-          //   _id: img._id,
-          //   request: {
-          //     type: "GET",
-          //     url: "http://localhost:8080/api/images" + img._id
-          //   }
           res.json(images);
           })
 });
@@ -58,8 +44,6 @@ router.get("/", (req, res, next) => {
 router.get("/:id", (req, res, next) => {
   let id = req.params.id;
   Image.findById(id)
-    .select("title genre path _id")
-    .exec()
     .then(image => {
       console.log("from database", image);
       if (image) {
@@ -81,50 +65,32 @@ router.post("/", upload.single("path"), (req, res, next) => {
   console.log(req.body);
   Image.create(
     new Image({
-      // _id: new mongoose.Types.ObjectId(),
+     
       title: req.body.title,
       genre: req.body.genre,
       path: req.file.path,
       votes: 0
     })
   ).then(entry => res.json(entry));
-  //   const image = new Image({
-  //     // _id: new mongoose.Types.ObjectId(),
-  //     title: req.body.title,
-  //     genre: req.body.genre,
-  //     path: req.file.path
-  //   });
-  //   console.log(image);
+
 });
 
 //works
 router.delete("/:id", (req, res) => {
   let id = req.params.id;
-  Image.findById(id)
-  .select("title genre path _id")
-    .exec()
-    .then(d => {
-      console.log("id states: ", d);
-      if (d) {
-        res.status(200).json({
-          d: d,
-          request: {
-            type: "DELETE",
-            url: "http://localhost:8080/api/images"
-          }
-        });
-      } else {
-        res;
-      }
+  Image.findOneAndDelete({_id: id})
+    .then(d => res.json(d));
     });
-});
+
 
 //works 
 router.put("/:picId", (req, res) => {
-  let updatePic = req.body;
-  let picId = req.params._id;
-  Image.findOneAndUpdate({ _id: picId }, updatePic, { new: true }).then(() => {
-      Image.find({}).then(img => res.json(img));
+  // let updatePic = req.body;
+  let picId = req.params.picId;
+  Image.find({ _id: picId }).then(image => {
+     image[0].votes = image[0].votes + 1
+     image[0].save()
+     res.json(image);
     }
   );
 });
