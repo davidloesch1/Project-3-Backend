42bdf4e7410d43dcf5d8d7cd7f78189ee091213b
Phil Morales <pirs.hlpoae.iml@gmail.com>
2020-01-29 22:22:45 -0500

    added some routes to both images and comments controllers

--- Controller/comments.js
+++ Controller/comments.js
@@ -1,20 +1,49 @@
 const express = require("express");
 const router = express.Router();
+const mongoose = require("mongoose");
 
 const Comments = require("../Model/CommentsSchema");
+const Image = require("../Model/ImageSchema");
 
+//this works
 //create all route below
 router.get("/", (req, res) => {
   Comments.find({}).then(comments => res.json(comments));
 });
 
+//this works
 router.post("/", (req, res) => {
-  //body-parser here with req.body.
-  //create new bookmark.
   let newComment = req.body;
   console.log(newComment);
-
-  Comments.create(req.body).then(comments => res.json(comments));
+  Comments.create(newComment).then(
+    Comments.find({}).then(comments => res.json(comments))
+  );
 });
 
-module.exports = router; 
+//not working
+//must connect the two ids on both schemas.
+router.post("/new", (req, res) => {
+  Image.create(req.body.image).then(image => {
+    Comments.create(req.body.comment).then(comments => {
+      image.comments.push(comment._id);
+      comments.subcomments.push(image._id);
+      image.save();
+      comments.save();
+
+      res.json(image);
+    });
+  });
+});
+
+//this one not working
+router.delete("/:id", (req, res) => {
+  let id = req.params.id;
+  Comments.findOneAndDelete({ id: id }).then(() => {
+    Comments.find({}).then(comments => {
+      console.log(comments);
+      res.json(comments);
+    });
+  });
+});
+
+module.exports = router;
--- Controller/images.js
+++ Controller/images.js
@@ -12,7 +12,12 @@ const storage = multer.diskStorage({
   }
 });
 const fileFilter = (req, file, cb) => {
-  if (file.mimetype === "image/jpeg" || file.mimetype === "image/png" || file.mimetype === "image/gif" || file.mimtype === "image/jpg") {
+  if (
+    file.mimetype === "image/jpeg" ||
+    file.mimetype === "image/png" ||
+    file.mimetype === "image/gif" ||
+    file.mimtype === "image/jpg"
+  ) {
     cb(null, true);
   } else {
     cb(new Error("filetype not supported"), false);
@@ -30,66 +35,104 @@ const Image = require("../Model/ImageSchema");
 
 router.get("/", (req, res, next) => {
   Image.find()
-  .select("title genre path _id")
-  .exec()
-  .then(images => {
-    const res = {
+    .select("title genre path _id")
+    .exec()
+    .then(images => {
+      const res = {
         count: images.length,
         images: images.map(img => {
-            return {
-                title: img.title,
-                genre: img.genre,
-                votes: img.votes,
-                _id: img._id,
-                request: {
-                    type: "GET",
-                    url: "http://localhost:8080/api/images" + img._id
-                }
+          return {
+            title: img.title,
+            genre: img.genre,
+            votes: img.votes,
+            _id: img._id,
+            request: {
+              type: "GET",
+              url: "http://localhost:8080/api/images" + img._id
             }
+          };
         })
-    }
-  })
+      };
+    });
 });
 
 router.get("/:id", (req, res, next) => {
-    let id = req.params.id
-    Image.findById(id)
+  let id = req.params.id;
+  Image.findById(id)
     .select("title genre path _id")
     .exec()
     .then(image => {
-        console.log("from database", image)
-        if(image) {
-            res.status(200).json({
-                image: image,
-                request: {
-                    type: 'GET',
-                    url: "http://localhost:8080/api/images"
-                }
-            })
-        } else{
-            res
-        }
+      console.log("from database", image);
+      if (image) {
+        res.status(200).json({
+          image: image,
+          request: {
+            type: "GET",
+            url: "http://localhost:8080/api/images"
+          }
+        });
+      } else {
+        res;
+      }
     });
-})
-  
-router.post("/", upload.single("path"), (req, res, next) => {
-  console.log(req.file)
-  console.log(req.body)
-    Image.create(new Image({
-    // _id: new mongoose.Types.ObjectId(),
-    title: req.body.title,
-    genre: req.body.genre,
-    path: req.file.path,
-    votes: 0
-  })).then(entry => res.json(entry));
-//   const image = new Image({
-//     // _id: new mongoose.Types.ObjectId(),
-//     title: req.body.title,
-//     genre: req.body.genre,
-//     path: req.file.path
-//   });
-//   console.log(image);
 });
+
+router.post("/", upload.single("path"), (req, res, next) => {
+  console.log(req.file);
+  console.log(req.body);
+  Image.create(
+    new Image({
+      // _id: new mongoose.Types.ObjectId(),
+      title: req.body.title,
+      genre: req.body.genre,
+      path: req.file.path,
+      votes: 0
+    })
+  ).then(entry => res.json(entry));
+  //   const image = new Image({
+  //     // _id: new mongoose.Types.ObjectId(),
+  //     title: req.body.title,
+  //     genre: req.body.genre,
+  //     path: req.file.path
+  //   });
+  //   console.log(image);
+});
+
+//many not work
+router.delete("/:id", (req, res) => {
+  let id = req.params.id;
+  Image.findById(id)
+    .select("title genre path _id")
+    .exec()
+    .then(d => {
+      console.log("id states: ", d);
+      if (d) {
+        res.status(200).json({
+          d: d,
+          request: {
+            type: "DELETE",
+            url: "http://localhost:8080/api/images"
+          }
+        });
+      } else {
+        res;
+      }
+    });
+});
+
+//update pic / may not working. 
+router.put("/:picId", (req, res) => {
+  let updatePic = req.body;
+  let picId = req.params._id;
+  Image.findOneAndUpdate({ _id: picId }, updatePic, { new: true }).then(() => {
+      Image.find({}).then(img => res.json(img));
+    }
+  );
+});
+
+
+
+
 
 module.exports = router;
 
