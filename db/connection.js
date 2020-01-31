69b727fe47bc97a143ce246aefa827b242d9b319
Phil Morales <pirs.hlpoae.iml@gmail.com>
2020-01-30 19:10:37 -0500

    update

--- db/connection.js
+++ db/connection.js
@@ -1,13 +1,7 @@
 const config = require('config');
 const db = config.get('mongoURI');
 
-// let mongoURI = "";
 
-// if (process.env.NODE_ENV === "production") {
-//     mongoURI = process.env.DB_URL;
-//   } else {
-//     mongoURI = "mongodb://localhost/photoappproject3";
-//   }
 
 //import mongoose
 const mongoose = require("mongoose");
